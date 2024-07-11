359fd01c69d7b0224940435090ba74c85eccb71e
Leyla Ayyubova <102313083+Leylayyubova@users.noreply.github.com>
2024-07-11 18:09:02 +0400

    nasproject

---
+++ src/js/swiper.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  });
+});
